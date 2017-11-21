b3197be9e9d1a0ab2de40282eefd11a0be13947a
sriram prakhya <sriramscience7@gmail.com>
2017-11-21 20:09:15 +0530

    z1: camera: Upgrade library dependencies for AOSP 8.0.0

--- camera/mm-image-codec/qomx_core/Android.mk
+++ camera/mm-image-codec/qomx_core/Android.mk
@@ -22,6 +22,6 @@ LOCAL_SRC_FILES := qomx_core.c
 
 LOCAL_MODULE           := libqomx_core
 LOCAL_PRELINK_MODULE   := false
-LOCAL_SHARED_LIBRARIES := libcutils libdl
+LOCAL_SHARED_LIBRARIES := lliblog ibcutils libdl
 
 include $(BUILD_SHARED_LIBRARY)
